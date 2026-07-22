869f4dbe868f315517e16757969c2091d18f9353
jobouf <jon2002bf@gmail.com>
2026-07-22 15:40:07 -0400

    Remove redundant WFO5001 mitgation measure for application color mode

--- src/Zebra_LabelProfile/Program.cs
+++ src/Zebra_LabelProfile/Program.cs
@@ -15,9 +15,7 @@ namespace Zebra_LabelProfile
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-#pragma warning disable WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
             Application.SetColorMode(SystemColorMode.System);
-#pragma warning restore WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
             ApplicationConfiguration.Initialize();
             var host = CreateHostBuilder().Build();
             ServiceProvider = host.Services;
